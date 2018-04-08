11b2a9efbd0687f09bf2d846f8aeb5ee48f596b7
Brandon Payton <brandon@happycode.net>
2018-04-08 15:15:14 -0700

    Add shared blocks to block completer

---
+++ edit-post/hooks/autocompleters/index.js
@@ -0,0 +1,44 @@
+/**
+ * WordPress dependencies.
+ */
+import { addFilter } from '@wordpress/hooks';
+import { select } from '@wordpress/data';
+import { createBlock } from '@wordpress/blocks';
+
+// Export for unit test
+export function getBlockOptions( query ) {
+	const editor = select( 'core/editor' );
+	const isInitialQuery = ! query;
+
+	// TODO: Fetch reusable blocks if necessary (they aren't fetched initially)
+	return isInitialQuery ?
+		// Before we have a query, offer frecent blocks as a sensible default.
+		editor.getFrecentInserterItems() :
+		editor.getInserterItems();
+}
+
+// Export for unit test
+export function getBlockCompletion( inserterItem ) {
+	const { name, initialAttributes } = inserterItem;
+	return {
+		action: 'replace',
+		value: createBlock( name, initialAttributes ),
+	};
+}
+
+// Export for unit test
+export function addReusableBlocksCompletion( completers ) {
+	const blocksCompleter = completers.find( c => 'blocks' === c.name );
+	if ( blocksCompleter ) {
+		blocksCompleter.options = getBlockOptions;
+		blocksCompleter.getOptionCompletion = getBlockCompletion;
+	}
+
+	return completers;
+}
+
+addFilter(
+	'blocks.Autocomplete.completers',
+	'core/edit-post/autocompleters/shared-blocks',
+	addReusableBlocksCompletion
+);
