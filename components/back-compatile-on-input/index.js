3b484985f40be6f7ff7a8bb662e005abc33c2636
Jorge <jorge.costa@developer.pt>
2018-04-10 14:15:09 +0100

    Fixed auto completer on IE
    Autocompleter did not worked on IE because IE does not handle onInput events on editables. We now make use of a newly implemented BackCompatibleOnInput component that falls back to key events when onInput is not supported.

---
+++ components/back-compatile-on-input/index.js
@@ -0,0 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+
+export default class BackCompatibleOnInput extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.onInput = this.onInput.bind( this );
+		this.onKeyUp = this.onKeyUp.bind( this );
+
+		this.state = {
+			isOnInputSupported: false,
+		};
+	}
+
+	onInput( event ) {
+		this.setState( {
+			onInputSupported: true,
+		} );
+		this.props.onInput( event );
+	}
+
+	onKeyUp( event ) {
+		this.props.onInput( event );
+	}
+
+	render() {
+		const { isOnInputSupported } = this.state;
+		const { onInput, children, ...props } = this.props;
+		return (
+			<div
+				onInput={ isOnInputSupported ? onInput : this.onInput }
+				onKeyUp={ isOnInputSupported ? undefined : this.onKeyUp }
+				{ ...props }
+			>
+				{ children }
+			</div>
+		);
+	}
+}
