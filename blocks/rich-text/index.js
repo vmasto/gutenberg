0fb2da4deea540ef5d98aa656372d5d3b3d7e0b1
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-04-12 14:26:26 +0200

    Blocks: Make sure that RichText controls are only displayed when block is selected This makes it backward compatible with the existing core blocks

--- blocks/rich-text/index.js
+++ blocks/rich-text/index.js
@@ -37,6 +37,7 @@ import TinyMCE from './tinymce';
 import { pickAriaProps } from './aria';
 import patterns from './patterns';
 import { EVENTS } from './constants';
+import { withBlockEditContext } from '../block-edit/context';
 
 const { BACKSPACE, DELETE, ENTER } = keycodes;
 
@@ -797,7 +798,7 @@ export class RichText extends Component {
 			placeholder,
 			multiline: MultilineTag,
 			keepPlaceholderOnFocus = false,
-			isSelected = false,
+			isSelected,
 			formatters,
 			autocompleters,
 		} = this.props;
@@ -824,7 +825,7 @@ export class RichText extends Component {
 
 		return (
 			<div className={ classes }>
-				{ ! inlineToolbar && (
+				{ isSelected && ! inlineToolbar && (
 					<BlockFormatControls>
 						{ formatToolbar }
 					</BlockFormatControls>
@@ -883,10 +884,13 @@ RichText.defaultProps = {
 };
 
 export default compose( [
-	withSelect( ( select ) => {
+	withBlockEditContext,
+	withSelect( ( select, { isSelected, blockEditContext } ) => {
 		const { isViewportMatch = identity } = select( 'core/viewport' ) || {};
+
 		return {
 			isViewportSmall: isViewportMatch( '< small' ),
+			isSelected: isSelected !== false && blockEditContext.isSelected,
 		};
 	} ),
 	withSafeTimeout,
